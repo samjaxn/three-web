34f96f169b87e206e5b75e0b43264132d6174d06
Sammy Jackson <sam.jackson@live.ca>
2019-10-11 11:29:51 -0400

    Updates

---
+++ precache-manifest.57e59d156ac16020b734a24870fcd714.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fb2cb2f0f2a115c9d6a5ceea035da66",
+    "url": "/three-web/index.html"
+  },
+  {
+    "revision": "7256a04544c161b8b2a7",
+    "url": "/three-web/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "bd7ede57e86d711c65db",
+    "url": "/three-web/static/js/2.0abce5f4.chunk.js"
+  },
+  {
+    "revision": "7256a04544c161b8b2a7",
+    "url": "/three-web/static/js/main.95f8f3d3.chunk.js"
+  },
+  {
+    "revision": "e6afa0093411c3619d57",
+    "url": "/three-web/static/js/runtime-main.9fe6b1f5.js"
+  },
+  {
+    "revision": "f3a233b36c5d84f4eaff4fecd21cd204",
+    "url": "/three-web/static/media/adamHead.f3a233b3.gltf"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/three-web/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "56987dd71209cdb8ad20e8ca9bc78b3d",
+    "url": "/three-web/static/media/tv2.56987dd7.gltf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ec341ccce96ead4fa169271246f75fa7.js"
+  "/three-web/precache-manifest.57e59d156ac16020b734a24870fcd714.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/three-web/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
