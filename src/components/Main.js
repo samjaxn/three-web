1896ec5ec1cfbf9810b9b1f249cf181975a30a05
Sammy Jackson <sam.jackson@live.ca>
2019-10-11 10:54:37 -0400

    added lights and click and drag controls

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,21 +1,32 @@
 import React, { Component } from 'react';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
-import { Scene, PerspectiveCamera, WebGLRenderer, BoxGeometry, MeshBasicMaterial, Mesh, AmbientLight } from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import { Scene, PerspectiveCamera, WebGLRenderer, BoxGeometry, MeshBasicMaterial, Mesh, AmbientLight, HemisphereLight, HemisphereLightHelper } from 'three';
 import tvGLTF from '../gltf/tv2.gltf'
 
 export class Main extends Component {
     componentDidMount(){
         var scene = new Scene();
-        var camera = new PerspectiveCamera( 50, window.innerWidth/window.innerHeight);
-
+        var camera = new PerspectiveCamera( 50, window.innerWidth/window.innerHeight, 0.1, 3000);
         var renderer = new WebGLRenderer();
         renderer.setSize( window.innerWidth, window.innerHeight );
+        var controls = new OrbitControls( camera, renderer.domElement );
         document.body.appendChild( renderer.domElement );
+        
 
         camera.position.z = 2000;
 
-        var light = new AmbientLight( 0xd0d0d0 ); // soft white light
+        var light = new AmbientLight( 0x000f0f ); // soft white light
         scene.add( light );
+        
+        
+        var hemiLight = new HemisphereLight( 0xffffff, 0xffffff, 0.6 );
+        hemiLight.color.setHSL( 0.6, 1, 0.6 );
+        hemiLight.groundColor.setHSL( 0.095, 1, 0.75 );
+        hemiLight.position.set( 0, 50, 0 );
+        scene.add( hemiLight );
+        var hemiLightHelper = new HemisphereLightHelper( hemiLight, 10 );
+        scene.add( hemiLightHelper );
 
         console.log(tvGLTF);
         let model;
@@ -38,10 +49,10 @@ export class Main extends Component {
         var animate = function () {
             requestAnimationFrame( animate );
 
-            if(model){
+            /*if(model){
                 model.rotation.x += 0.01;
                 model.rotation.y += 0.01;
-            }
+            }*/
             
 
             renderer.render( scene, camera );
