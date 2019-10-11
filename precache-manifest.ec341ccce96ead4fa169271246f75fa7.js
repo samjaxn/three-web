d7383ed8710cd811db9b881e567afd786f548886
Sammy Jackson <sam.jackson@live.ca>
2019-10-11 11:27:23 -0400

    Updates

---
+++ precache-manifest.ec341ccce96ead4fa169271246f75fa7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7b5edf1c1d8d52beb5f0c66df1d6220",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a1c0526f5547a2de653c",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "bd7ede57e86d711c65db",
+    "url": "/static/js/2.0abce5f4.chunk.js"
+  },
+  {
+    "revision": "a1c0526f5547a2de653c",
+    "url": "/static/js/main.9f771244.chunk.js"
+  },
+  {
+    "revision": "9efcef7fe769e008658d",
+    "url": "/static/js/runtime-main.22258917.js"
+  },
+  {
+    "revision": "f3a233b36c5d84f4eaff4fecd21cd204",
+    "url": "/static/media/adamHead.f3a233b3.gltf"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "56987dd71209cdb8ad20e8ca9bc78b3d",
+    "url": "/static/media/tv2.56987dd7.gltf"
+  }
+]);
